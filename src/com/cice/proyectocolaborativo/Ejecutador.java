717e5352099be171d44a13a3cef40772232edc21
Rogerpcb <63349456+Rogerpcb@users.noreply.github.com>
2020-04-11 17:30:38 +0200

    Second Commit

--- src/com/cice/proyectocolaborativo/Ejecutador.java
+++ src/com/cice/proyectocolaborativo/Ejecutador.java
@@ -3,7 +3,7 @@ package com.cice.proyectocolaborativo;
 public class Ejecutador {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.println("fdsa");
 
 	}
 
